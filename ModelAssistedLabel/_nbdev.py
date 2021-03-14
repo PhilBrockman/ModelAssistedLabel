b891826e10c8eea96f91093f9094e8394ca971f3
Phil Brockman <phil.brockman@gmail.com>
2021-03-14 23:55:41 +0000

    updating docs

--- ModelAssistedLabel/_nbdev.py
+++ ModelAssistedLabel/_nbdev.py
@@ -2,9 +2,10 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"FileUtilities": "01_train.ipynb",
+         "Generation": "01_train.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://PhilBrockman.github.io/ModelAssistedLabel/"
 
