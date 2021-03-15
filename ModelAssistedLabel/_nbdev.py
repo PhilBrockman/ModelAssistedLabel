31a50562288eaae60ceb0d316fcd631b734226bf
Phil Brockman <phil.brockman@gmail.com>
2021-03-15 15:56:33 +0000

    updating docs

--- ModelAssistedLabel/_nbdev.py
+++ ModelAssistedLabel/_nbdev.py
@@ -2,8 +2,7 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"Defaults": "00_core.ipynb",
-         "FileUtilities": "01_split.ipynb",
+index = {"FileUtilities": "01_split.ipynb",
          "Generation": "01_split.ipynb"}
 
 modules = ["core.py"]
